1d12f1119800a56d9cfdbbb848ea5e3f6b33078c
Encryptor <Encryptor@DESKTOP-7FHMOB6>
2016-09-12 21:56:49 +0300

    Added CalculatePage

---
+++ BaseM/BaseM/CalculatorPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace BaseM
+{
+    public partial class CalculatorPage : ContentPage
+    {
+        public CalculatorPage()
+        {
+            InitializeComponent();
+        }
+
+        private void okayButton_Clicked(object sender, EventArgs e)
+        {
+            resultLabel.Text = Translator.Translate(numberEntry.Text, NumericSystem.Decimal, NumericSystem.Binary);
+        }
+    }
+}
