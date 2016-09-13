5b0aac78ef7378671456cc487cee3da5f96af856
Encryptor <Encryptor@DESKTOP-7FHMOB6>
2016-09-13 06:11:47 +0300

    Added a converter from string to NumberSystem

---
+++ BaseM.Tests/HelperMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseM.Tests
+{
+    public static class HelperMethods
+    {
+        public static NumericSystem StringToNumericSystem(string system)
+        {
+            switch(system.ToUpper())
+            {
+                case "BINARY":
+                    return NumericSystem.Binary;
+                case "OCTAL":
+                    return NumericSystem.Octal;
+                case "DECIMAL":
+                    return NumericSystem.Decimal;
+                case "HEXADECIMAL":
+                    return NumericSystem.Hexadecimal;
+            }
+
+            throw new ArgumentException("Unknown system");
+        }
+    }
+}
