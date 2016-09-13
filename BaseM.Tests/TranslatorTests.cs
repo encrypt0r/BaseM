656ccdc9db16f538b71cdb477e476fbfcdc94f10
Encryptor <Encryptor@DESKTOP-7FHMOB6>
2016-09-13 05:12:25 +0300

    Added Test Project and a sample test

---
+++ BaseM.Tests/TranslatorTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseM.Tests
+{
+    [TestClass]
+    public class TranslatorTests
+    {
+        [TestMethod]
+        public void DecimalToDecimalTest1()
+        {
+            string result = Translator.Translate("100", NumericSystem.Decimal, NumericSystem.Decimal);
+            Assert.AreEqual(result, "100");
+        }
+    }
+}
