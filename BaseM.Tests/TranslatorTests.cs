1a0decafb415bc673f2a175b31aab04e9fd95164
Encryptor <Encryptor@DESKTOP-7FHMOB6>
2016-09-13 06:17:33 +0300

    added a data driven method to translator tests

--- BaseM.Tests/TranslatorTests.cs
+++ BaseM.Tests/TranslatorTests.cs
@@ -6,12 +6,14 @@ namespace BaseM.Tests
     [TestClass]
     public class TranslatorTests
     {
+        public TestContext TestContext { get; set; }
+
         [TestMethod]
         [TestCategory("Translator")]
         public void DecimalToDecimalSimple()
         {
             string result = Translator.Translate("100", NumericSystem.Decimal, NumericSystem.Decimal);
-            Assert.AreEqual(result, "100");
+            Assert.AreEqual("100", result);
         }
 
         [TestMethod]
@@ -20,7 +22,35 @@ namespace BaseM.Tests
         {
             string bigNum = "1000000000000000000000000000000000";
             string result = Translator.Translate(bigNum, NumericSystem.Decimal, NumericSystem.Decimal);
-            Assert.AreEqual(result, Translator.UNKNOWN);
+            Assert.AreEqual(Translator.UNKNOWN, result);
         }
+
+        [TestMethod]
+        [TestCategory("Translator")]
+        public void DecimalToBinarySimple()
+        {
+            string number = "25";
+            string output = "11001";
+            string result = Translator.Translate(number, NumericSystem.Decimal, NumericSystem.Decimal);
+            Assert.AreEqual(output, result);
+        }
+
+        [TestMethod]
+        [TestCategory("Translator")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+            "|DataDirectory|\\NumberConversionTests.xml", "convert",
+            DataAccessMethod.Sequential)]
+        public void VigorousTest()
+        {
+            var number = TestContext.DataRow["number"].ToString();
+            var expectedResult = TestContext.DataRow["result"].ToString();
+            var from = HelperMethods.StringToNumericSystem(TestContext.DataRow["from"].ToString());
+            var to = HelperMethods.StringToNumericSystem(TestContext.DataRow["to"].ToString());
+
+            var result = Translator.Translate(number, from, to);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        
     }
 }
