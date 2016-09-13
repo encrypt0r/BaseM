dc99399568c39ee02990cf62d1c931eb36aa5792
Encryptor <Encryptor@DESKTOP-7FHMOB6>
2016-09-13 05:44:39 +0300

    Added tests for too big numbers

--- BaseM.Tests/TranslatorTests.cs
+++ BaseM.Tests/TranslatorTests.cs
@@ -7,10 +7,20 @@ namespace BaseM.Tests
     public class TranslatorTests
     {
         [TestMethod]
-        public void DecimalToDecimalTest1()
+        [TestCategory("Translator")]
+        public void DecimalToDecimalSimple()
         {
             string result = Translator.Translate("100", NumericSystem.Decimal, NumericSystem.Decimal);
             Assert.AreEqual(result, "100");
+        }
+
+        [TestMethod]
+        [TestCategory("Translator")]
+        public void DecimalToDecimalBigNumber()
+        {
+            string bigNum = "1000000000000000000000000000000000";
+            string result = Translator.Translate(bigNum, NumericSystem.Decimal, NumericSystem.Decimal);
+            Assert.AreEqual(result, Translator.UNKNOWN);
         }
     }
 }
